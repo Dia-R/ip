9d93345251fdb50eb0c3a18b19f207cd050f80d5
Dia-R <nadiarahmanchocolate@gmail.com>
2026-01-24 13:19:16 +0800

    feat: implement Level-0: Rename, Greet, Exit tasks

---
+++ src/main/java/Cat.java
@@ -0,0 +1,8 @@
+public class Cat {
+    public static void main(String[] args) {
+        String hello = "Hello, furrr-iend! Do you need a helping paw?";
+        String bye = "Aww, see mew next time!";
+        System.out.println(hello);
+        System.out.println(bye);
+    }
+}
