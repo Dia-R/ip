e7121283abbe200a3f22776ea7f48d2c7f44442c
Dia-R <nadiarahmanchocolate@gmail.com>
2026-01-25 21:13:52 +0800

    feat: implement Level-1: Echo task

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,14 +1,23 @@
+import java.util.Scanner;
+
 public class ChatBot {
     private String name;
     private boolean isRunning;
+    private Scanner scanner;
 
     public ChatBot(String name) {
         this.name = name;
         this.isRunning = true;
+        this.scanner = new Scanner(System.in);
     }
 
     public void run(){
         helloUser();
+
+        while (isRunning) {
+            String userCommand = scanner.nextLine();
+            executeCommand(userCommand);
+        }
         byeUser();
     }
 
@@ -19,5 +28,13 @@ public class ChatBot {
     private void byeUser() {
         String hello = "Aww, see mew next time!";
         System.out.println(hello);
+    }
+
+    private void executeCommand(String userCommand) {
+        if (userCommand.equalsIgnoreCase("bye")) {
+            isRunning = false;
+        } else {
+            System.out.println(userCommand);
+        }
     }
 }
